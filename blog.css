574eefb93bcc51de612e3665f552bcd9578129dc
stink-head <cobielliott@gmail.com>
2017-05-19 13:30:26 +0100

    Add files via upload

---
+++ blog.css
@@ -0,0 +1,70 @@
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center 
+}
+
+#sadguy {
+  padding-right:10px;
+}
+
+#happyguy {
+  padding-left:10px;
+}
+
+#menucontainer ul {
+ margin-top:1em;
+ margin-bottom:1em;
+ padding-left: 0;
+}
+
+.container1 {
+  display: inline-block;
+}
+
+#menulist li {
+display: inline; 
+padding-right: 5px;
+}
+
+
+#footnote {
+  font-family: arial;
+  font-size: 10px;
+  color: pink;
+  margin-top: 80px;
+}
+
+h3 {
+  margin-bottom:2em;
+  margin-top:4em;
+  font-family: 'Amatic SC', cursive;
+  color: #e265cc;
+  font-size:20px;
+}
+
+a {
+  color: #5cd4ef;
+  font-size:30px;
+  text-decoration:none;
+  font-family: 'Amatic SC', cursive;
+}
+
+  a:hover {
+  font-weight:bold;
+  color: #e265cc;
+}
+
+
+body {
+  text-align:center;
+  background-color:white;
+}
+
+
+#back-soon {
+  margin-top:50px;
+  color:grey;
+  font-size: 12px;
+  font-family: 'PT Mono', monospace;
+}
